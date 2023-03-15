74db216439d568dd0915ec73a9c6d8685b0ec889
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2023-03-15 22:05:11 +0100

    Saving OpenAPI Spec to files

--- src/test/java/org/lafeuille/demo/OpenApiSpecTest.java
+++ src/test/java/org/lafeuille/demo/OpenApiSpecTest.java
@@ -1,10 +1,18 @@
 package org.lafeuille.demo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -15,21 +23,45 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class OpenApiSpecTest {
 
+    private static final Path outputDir = Paths.get("build", "generated", "docs");
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        Files.createDirectories(outputDir);
+    }
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void download_openapi_json() throws Exception {
-        mockMvc.perform(get("/v3/api-docs"))
+        var bytes = mockMvc.perform(get("/v3/api-docs"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("openapi").value("3.0.1"))
-                .andDo(print());
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsByteArray();
+
+        copyToPath(bytes, outputDir.resolve("openapi.json"));
     }
 
     @Test
     void download_openapi_yaml() throws Exception {
-        mockMvc.perform(get("/v3/api-docs.yaml"))
+        var bytes = mockMvc.perform(get("/v3/api-docs.yaml"))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsByteArray();
+
+        copyToPath(bytes, outputDir.resolve("openapi.yaml"));
+    }
+
+    private void copyToPath(byte[] bytes, Path target) throws IOException {
+        Files.deleteIfExists(target);
+        try (InputStream inputStream = new ByteArrayInputStream(bytes)) {
+            Files.copy(inputStream, target);
+        }
     }
 }
