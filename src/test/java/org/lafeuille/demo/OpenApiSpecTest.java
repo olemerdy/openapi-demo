1180d1139caa53d25ccc5885e32c56b0bd264661
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2023-03-15 21:21:59 +0100

    Adding content

--- src/test/java/org/lafeuille/demo/OpenApiSpecTest.java
+++ src/test/java/org/lafeuille/demo/OpenApiSpecTest.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,12 +22,14 @@ public class OpenApiSpecTest {
     void download_openapi_json() throws Exception {
         mockMvc.perform(get("/v3/api-docs"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("openapi").value("3.0.1"));
+                .andExpect(jsonPath("openapi").value("3.0.1"))
+                .andDo(print());
     }
 
     @Test
     void download_openapi_yaml() throws Exception {
         mockMvc.perform(get("/v3/api-docs.yaml"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
