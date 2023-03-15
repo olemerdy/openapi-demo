b8b9d48e453625c35710782aa3c7048829347b0e
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2023-03-15 21:10:31 +0100

    Introduce SpringDoc

---
+++ src/test/java/org/lafeuille/demo/OpenApiSpecTest.java
@@ -0,0 +1,34 @@
+package org.lafeuille.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OpenApiSpecTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void download_openapi_json() throws Exception {
+        mockMvc.perform(get("/v3/api-docs"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("openapi").value("3.0.1"));
+    }
+
+    @Test
+    void download_openapi_yaml() throws Exception {
+        mockMvc.perform(get("/v3/api-docs.yaml"))
+                .andExpect(status().isOk());
+    }
+}
